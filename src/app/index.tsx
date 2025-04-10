01c189670036a7007143f92e4040398e9427063e
jrodrigojrs <jrod_br@outlook.com>
2025-04-09 21:29:50 -0300

    Nativewind install

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+import "../../global.css";
+
+export default function App() {
+ return (
+  <View className="flex-1 items-center justify-center">
+   <Text>Open up App.tsx to start working on your app!</Text>
+  </View>
+ );
+}
